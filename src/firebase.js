91aa2513c7b5d04a7e7ee885a0960338ebfa32ba
Mohammed Taher <mtaher@elm.sa>
2020-09-10 22:52:53 +0300

    Add Header Navigation Bar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyK64bOHl-4iMOP0DkGKtt_N9RmctIRko",
+  authDomain: "clone-f9b7c.firebaseapp.com",
+  databaseURL: "https://clone-f9b7c.firebaseio.com",
+  projectId: "clone-f9b7c",
+  storageBucket: "clone-f9b7c.appspot.com",
+  messagingSenderId: "462713524041",
+  appId: "1:462713524041:web:69ec1b3b86769885f99498",
+};
